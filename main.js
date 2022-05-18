c56c934be87b0524ba4497a09602ef1d3771b606
J_Klakus <98471049+JojokCreator@users.noreply.github.com>
2022-05-18 07:40:23 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// Code Plan
+/* 
+Stage 1
+- Correct HTML and CSS to be the same as high fidelity wireframe
+- Using CSS variables and responsive design
+
+Stage 2
+- Add hover over buttons with different effects
+- Check on different screen sizes
+- Use lighthouse
+
+Stage 3
+- Dark mode
+
+Stage 4 (unlikely to hit)
+- mobile menus
+
+*/
